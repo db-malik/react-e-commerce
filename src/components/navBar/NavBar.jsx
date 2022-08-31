0eca06b9cd84c7877b4f0f143f8a189698b4b50b
Malik <95544411+db-malik@users.noreply.github.com>
2022-09-01 00:44:14 +0100

    create home  page and navBar component

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+// style
+const Container = styled.div`
+	height: 60px;
+	background-color: black;
+`;
+
+// style
+const NavBar = () => {
+	return <Container>NavBar</Container>;
+};
+
+export default NavBar;
