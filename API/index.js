dc0f6486361ea93f23171eb8e286346fff3091ce
Malik <95544411+db-malik@users.noreply.github.com>
2022-09-12 21:58:30 +0100

    add cryptoJs

--- API/index.js
+++ API/index.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const app = express();
-
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
 mongoose
 	.connect(process.env.MONGO_URL)
-	.then(() => {
-		console.log("db connection Successfully established");
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+	.then(() => console.log("db connection Successfully established"))
+	.catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log("server is running , listening on port 5000");
