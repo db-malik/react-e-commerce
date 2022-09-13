0d38a8511225d4f435c9f516c113ebf30c6e8ce0
Malik <95544411+db-malik@users.noreply.github.com>
2022-09-13 02:17:16 +0100

    add access token

--- API/routes/auth.js
+++ API/routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -36,11 +37,21 @@ router.post("/login", async (req, res) => {
 		const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
 		const inputPassword = req.body.password;
+
 		originalPassword != inputPassword &&
 			res.status(401).json("wrong credential");
 
+		const accessToken = jwt.sign(
+			{
+				id: user._id,
+				isAdmin: user.isAdmin,
+			},
+			process.env.JWT_KEY,
+			{ expiresIn: "2d" }
+		);
+
 		const { password, ...others } = user._doc;
-		res.status(200).json(others);
+		res.status(200).json({ ...others, accessToken });
 	} catch (error) {
 		res.status(500).json(err);
 	}
