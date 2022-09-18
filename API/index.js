f8942543758481d9f29ef61d7207b4cb7a3e62eb
db-malik <dbmalek92@gmail.com>
2022-09-19 00:17:28 +0100

    user update

--- API/index.js
+++ API/index.js
@@ -1,23 +1,23 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
-const userRoute = require("./routes/user");
-const authRoute = require("./routes/auth");
+const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
 
-dotenv.config();
+dotenv.config()
 
 mongoose
-	.connect(process.env.MONGO_URL)
-	.then(() => console.log("db connection Successfully established"))
-	.catch((err) => console.log(err));
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('db connection Successfully established'))
+  .catch((err) => console.log(err))
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
+app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
 
-app.listen(process.env.PORT || 5000, () => {
-	console.log("server is running , listening on port 5000");
-});
+app.listen(process.env.PORT || 4000, () => {
+  console.log('server is running , listening on port 4000')
+})
--- API/routes/user.js
+++ API/routes/user.js
@@ -1,3 +1,32 @@
-const router = require("express").Router();
+const User = require('../models/User')
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require('./verifyToken')
 
-module.exports = router;
+const router = require('express').Router()
+
+//UPDATE
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_KEY
+    ).toString()
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    )
+    res.status(200).json(updatedUser)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+module.exports = router
